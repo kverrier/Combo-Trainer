6fcb8ad9ef609654bf0317e15c7a98e2e4233e0e
Kyle Verrier <kyle.verrier@gmail.com>
2012-11-12 14:16:06 -0500

    compile assets fix

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,7 @@ module Trainer
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.compress = true
+
   end
 end
